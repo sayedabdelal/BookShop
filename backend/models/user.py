5b5a81df38eeb6533a908c61e261f26182fb5ed2
youssef <youssefahmedabdelkader25@gmail.com>
2024-08-31 02:16:13 +0300

    user model DB

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,17 +1,29 @@
-from flask_sqlalchemy import SQLAlchemy
-import bcrypt
 from backend import db
+import bcrypt
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    carts = db.relationship('Cart', back_populates='user', lazy=True)  # Use back_populates here
 
-    def _init_(self, fname, email, password):
+    def __init__(self, fname, email, password):
         self.fname = fname
         self.email = email
         self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
     def check_password(self, password):
         return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'fname': self.fname,
+            'lname': self.lname,
+            'username': self.username,
+            'email': self.email,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'carts': [{'id': cart.id, 'total_price': cart.total_price} for cart in self.carts]
+        }
