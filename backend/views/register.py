06b3542168a99dc1753c593623bdd8ef47693749
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-02 02:26:28 +0300

    Fix: Solve login page bug by passing password to User not hashed password Because we hash the password when go to database in User model so we dont

--- backend/views/register.py
+++ backend/views/register.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, request, jsonify
-# from models.user_model import db, User
 from backend import db
 from backend.models.user import User
-import bcrypt
 
 register = Blueprint('register', __name__)
 
@@ -12,17 +10,15 @@ def register_view():
     data = request.get_json()
     fname = data.get('name')
     email = data.get('email')
-    password1 = data.get('password')
+    password = data.get('password')
     
-    if not fname or not email or not password1:
+    if not fname or not email or not password:
         return jsonify({'error': 'All fields are required'}), 400
 
     if User.query.filter_by(email=email).first():
         return jsonify({'error': 'Email already registered'}), 400
-
-    hashed_password = bcrypt.hashpw(password1.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
     
-    new_user = User(fname=fname, email=email, password=hashed_password)
+    new_user = User(fname=fname, email=email, password=password)
     db.session.add(new_user)
     db.session.commit()
 
