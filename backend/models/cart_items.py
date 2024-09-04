95871dd9ff5e906d174260d74be657d26c03ba82
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-04 22:17:01 +0300

    Add relationshipt to cartitems with cart and book

--- backend/models/cart_items.py
+++ backend/models/cart_items.py
@@ -1,17 +1,16 @@
 from backend import db
-from sqlalchemy.sql import func
 from uuid import uuid4
 
-
-
 class CartItems(db.Model):
-    __tablename__ = 'CartItems'
+    __tablename__ = 'cart_items'
     id = db.Column(db.String(32), primary_key=True, default=lambda: uuid4().hex)
     cart_id = db.Column(db.String(32), db.ForeignKey('cart.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False, default=1)
-    book = db.relationship('Book', backref='cart_items', lazy=True)
+
+    cart = db.relationship('Cart', back_populates='items')
+    book = db.relationship('Book', back_populates='cart_items')
 
     def __init__(self, quantity, cart_id, user_id, book_id):
         self.quantity = quantity
