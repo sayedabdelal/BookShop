e01a1f6361a02ca9ea70a816202487fbc46e822b
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-04 21:23:17 +0300

    Update: allow credentials and add blueprint for cartItems and wishlist, etc...

--- backend/__init__.py
+++ backend/__init__.py
@@ -8,10 +8,9 @@ DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, supports_credentials=True)
     app.config['SECRET_KEY'] = 'b1c7960dce797a332056f8347d56439b'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    
     db.init_app(app)
 
     from .views.login import login as login_blueprint
@@ -22,6 +21,9 @@ def create_app():
     from .views.cart import cart as cart_blueprint
     from .views.book import book as book_blueprint
     from .views.category import category as category_blueprint
+    from .views.cart_items import cart_items as cart_items_blueprint
+    from .views.shop import shop as shop_blueprint
+    from .views.wishlist import wishlist as wishlist_blueprint
 
     app.register_blueprint(login_blueprint, url_prefix='/')
     app.register_blueprint(register_blueprint, url_prefix='/')
@@ -31,6 +33,9 @@ def create_app():
     app.register_blueprint(cart_blueprint, url_prefix='/')
     app.register_blueprint(book_blueprint, url_prefix='/')
     app.register_blueprint(category_blueprint, url_prefix='/')
+    app.register_blueprint(cart_items_blueprint, url_prefix='/')
+    app.register_blueprint(shop_blueprint, url_prefix='/')
+    app.register_blueprint(wishlist_blueprint, url_prefix='/')
 
     with app.app_context():
         db.create_all()
