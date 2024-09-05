e2be8d73a50d20804a45cc100c959c3eadcc4cbe
root <root@sun>
2024-09-05 12:08:45 +0200

    Implement logout functionality with session clearing and UI updates

--- src/util/http.js
+++ src/util/http.js
@@ -195,6 +195,21 @@ export async function submitCheckout(formData, cartItems) {
   return response.json();  
 }
 
+
+export async function logoutUser() {
+  const response = await fetch('http://127.0.0.1:5000/logout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include', // Ensure cookies are sent with the request
+  });
+  console.log("log status--->" ,response.status)
+
+  if (!response.ok) {
+    throw new Error('Logout failed');
+  }
+}
  
  
 
