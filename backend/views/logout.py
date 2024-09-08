bed7c849ef92725072361a7e340508f00d3fadb3
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-08 21:28:25 +0300

    debug: add print statments to debug the session is work correctly

--- backend/views/logout.py
+++ backend/views/logout.py
@@ -4,8 +4,10 @@ logout = Blueprint('logout', __name__)
 
 @logout.route('/logout', methods=['POST', 'GET'])
 def logout_view():
+    print(f"Session before logout: {session}")
     if 'user_id' in session:
         session.pop('user_id', None)
+        print(f"Session after logout: {session}")
         return jsonify({'message': 'Logout successful'}), 200
     else:
         return jsonify({'error': 'You are not logged in'}), 401
