e371335a14a9d87ff5805e4c7cfcb5510421e193
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-08 21:31:43 +0300

    update: add blueprint for deatils and clean the file

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from os import path
+from flask_session import Session
+# import redis
 
 db = SQLAlchemy()
+session = Session()
 DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, supports_credentials=True)
+    CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'b1c7960dce797a332056f8347d56439b'
+    # Stores sessions in files by default
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+    app.config['SESSION_COOKIE_SECURE'] = True
+    # using redis instead of filesystem
+    # app.config['SESSION_REDIS'] = redis.from_url('redis://127.0.0.1:6379')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    session.init_app(app)
     db.init_app(app)
 
     from .views.login import login as login_blueprint
@@ -24,6 +34,7 @@ def create_app():
     from .views.cart_items import cart_items as cart_items_blueprint
     from .views.shop import shop as shop_blueprint
     from .views.wishlist import wishlist as wishlist_blueprint
+    from .views.details import details as details_blueprint
 
     app.register_blueprint(login_blueprint, url_prefix='/')
     app.register_blueprint(register_blueprint, url_prefix='/')
@@ -36,8 +47,9 @@ def create_app():
     app.register_blueprint(cart_items_blueprint, url_prefix='/')
     app.register_blueprint(shop_blueprint, url_prefix='/')
     app.register_blueprint(wishlist_blueprint, url_prefix='/')
+    app.register_blueprint(details_blueprint, url_prefix='/')
 
     with app.app_context():
         db.create_all()
-
+        
     return app
