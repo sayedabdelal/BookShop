5700526e426bac92a87d47b96067308d0933a08c
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-08 22:08:55 +0300

    feat: mereg backend startup and data loading in single script

--- run_backend.py
+++ run_backend.py
@@ -1,7 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+import sys
 from backend import create_app
+from backend.models.book import load_book_data
 
 app = create_app()
 
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == 'load':
+        load_book_data()
+    else:
+        app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
