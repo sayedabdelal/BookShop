0c380efec0217c7f6a2bc814a2b9c05ebd63dd85
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-08 21:27:29 +0300

    rm: remove unnessassery imports and return user_id with response

--- backend/views/login.py
+++ backend/views/login.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, request, jsonify, session
-# from models.user_model import db, User
-from backend import db
 from backend.models.user import User
 
 
@@ -8,18 +6,20 @@ login = Blueprint('login', __name__)
 
 @login.route('/login', methods=['POST'])
 def login_view():
-    if request.method == 'POST':
-        data = request.get_json()  # Use get_json() for JSON payload
-        email = data.get('email')
-        password = data.get('password')
-        
-        if not email or not password:
-            return jsonify({'error': 'All fields are required'}), 400
-        
-        user = User.query.filter_by(email=email).first()
-        if user and user.check_password(password):
-            session['user_id'] = user.id
-            return jsonify({'message': 'Login successful!'}), 200
-        else:
-            return jsonify({'error': 'Invalid email or password'}), 401
-    return jsonify({'message': 'Please provide login credentials.'}), 400
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    
+    if not email or not password:
+        return jsonify({'error': 'All fields are required'}), 400
+    
+    user = User.query.filter_by(email=email).first()
+    if user and user.check_password(password):
+        session['user_id'] = user.id
+        print(session)
+
+        print(f"Session after login: {session}")
+        response = jsonify({'message': 'Login successful!', 'user_id': user.id})
+        return response, 200
+    else:
+        return jsonify({'error': 'Invalid email or password'}), 401
