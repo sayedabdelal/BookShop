735169e29c9dda5e2d95f65980ebc61a1c6fd939
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-08 21:19:49 +0300

    feat: add realtionship with Book, return book data in to_dict

--- backend/models/wishlist.py
+++ backend/models/wishlist.py
@@ -5,6 +5,12 @@ class Wishlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    book = db.relationship('Book', backref='wishlist_item')
 
     def to_dict(self):
-        return {'id': self.id, 'user_id': self.user_id, 'book_id': self.book_id}
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'book_id': self.book_id,
+            'book': self.book.to_dict() if self.book else None
+            }
