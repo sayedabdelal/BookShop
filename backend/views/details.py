f1b7d108b394779a080b4fb637bdde9cce3798c6
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-09 22:21:47 +0300

    Update: make the use can see the details of book without check in session for his user_id

--- backend/views/details.py
+++ backend/views/details.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, session
+from flask import Blueprint, jsonify
 from backend.models.book import Book
 
 details = Blueprint('details', __name__)
@@ -7,10 +7,6 @@ details = Blueprint('details', __name__)
 @details.route('/api/details/<int:book_id>', methods=['GET'])
 def get_details(book_id):
     '''Get data of book to show it'''
-    user_id = session.get('user_id')
-    if not user_id:
-        return jsonify({'error': 'Unauthorized'}), 401
-
     book = Book.query.filter_by(id=book_id).first()
 
     if book:
