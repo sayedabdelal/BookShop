014a15ae7f278170b073688a73add52476c2db9a
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-12 04:10:29 +0300

    update: fix bugs for admin check session

--- backend/views/logout.py
+++ backend/views/logout.py
@@ -5,9 +5,16 @@ logout = Blueprint('logout', __name__)
 @logout.route('/logout', methods=['POST', 'GET'])
 def logout_view():
     print(f"Session before logout: {session}")
-    if 'user_id' in session:
+
+    if 'admin_id' in session:
+        session.pop('admin_id', None)
+        print(f"Session after admin logout: {session}")
+        return jsonify({'message': 'Admin logout successful'}), 200
+
+    elif 'user_id' in session:
         session.pop('user_id', None)
         print(f"Session after logout: {session}")
         return jsonify({'message': 'Logout successful'}), 200
+    
     else:
         return jsonify({'error': 'You are not logged in'}), 401
