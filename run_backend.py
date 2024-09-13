ae0e86b1156ca71d8cdce49df64fd0cd04d92bf1
aylaaoufi <116092909+youssef3092004@users.noreply.github.com>
2024-09-13 04:18:26 +0300

    documentation the run the backend

--- run_backend.py
+++ run_backend.py
@@ -4,6 +4,27 @@ import sys
 from backend import create_app
 from backend.models.book import load_book_data
 
+"""
+Script to run the Flask application or load initial book data.
+
+Usage:
+- `python run_backend.py`: Runs the Flask application in debug mode.
+- `python run_backend.py load`: Loads initial book data into the database.
+
+Dependencies:
+- `sys`: For command-line argument parsing.
+- `backend`: For creating the Flask application and loading book data.
+
+Functions:
+- `main() -> None`: Main function to handle command-line arguments and run the application.
+
+    Command-line Arguments:
+    - `load`: If provided, the script will execute `load_book_data()` to load book data into the database.
+    - Otherwise, it will run the Flask application.
+
+If executed as the main module, `main()` is called to determine the appropriate action.
+"""
+
 app = create_app()
 
 def main():
