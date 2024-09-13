3341acdb8cc6b5018c7070b3b01f60c700f8922e
aylaaoufi <116092909+youssef3092004@users.noreply.github.com>
2024-09-13 04:07:14 +0300

    documentation the view dashboard

--- backend/views/dashboard.py
+++ backend/views/dashboard.py
@@ -1,11 +1,20 @@
 from flask import Blueprint, jsonify, session
 from backend.models.user import User
 
-
 dashboard = Blueprint('dashboard', __name__)
 
 @dashboard.route('/dashboard', methods=['GET'])
 def dashboard_view():
+    '''Retrieve user information for the dashboard.
+
+    This route provides the logged-in user's details such as ID, name, and email.
+
+    Returns:
+        Response: 
+            - **200 OK**: A JSON object containing the user data (ID, name, email) if the user is found.
+            - **401 Unauthorized**: If the user is not logged in (no email in session).
+            - **404 Not Found**: If no user is found with the email in the session.
+    '''
     if 'email' in session:
         user = User.query.filter_by(email=session['email']).first()
         if user:
