83c79ea1360c872174b873a9db5fd3b8d7e6685e
aylaaoufi <116092909+youssef3092004@users.noreply.github.com>
2024-09-13 04:06:11 +0300

    documentation the view category

--- backend/views/category.py
+++ backend/views/category.py
@@ -2,24 +2,32 @@ from flask import Blueprint, request, jsonify
 from backend import db
 from ..models.category import Category
 
-
 category = Blueprint('category', __name__)
 
-@category.route('/category')
+@category.route('/category', methods=['GET'])
 def get_all_categories():
+    '''Retrieve all categories.
+
+    This route fetches all categories from the database.
+
+    Returns:
+        Response: A JSON list of categories if found, or a message indicating no categories are found.
+    '''
     categories = Category.query.all()
     json_categories = list(map(lambda c: c.to_dict(), categories))
     return jsonify({"categories": json_categories}), 200
 
-
-# @category.route('/category/<string:name>')
-# def all_books_in_category(name):
-#     books = Category.query.filter_by(name=name).all()
-#     json_books = list(map(lambda b: b.to_json(), books))
-#     return jsonify({"category": json_books}), 200
-
 @category.route('/category', methods=['POST'])
 def create_category():
+    '''Create a new category.
+
+    JSON Body:
+        name (str): The name of the category.
+        description (str, optional): A description of the category.
+
+    Returns:
+        Response: The created category details if created successfully, or an error message if required fields are missing or the input is invalid.
+    '''
     data = request.get_json()
     
     if not data or 'name' not in data:
