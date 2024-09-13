ed47c57d5c8263bf08d9aac3b6bf8d732efd33eb
aylaaoufi <116092909+youssef3092004@users.noreply.github.com>
2024-09-13 03:59:02 +0300

    documentation the modle

--- backend/models/wishlist.py
+++ backend/models/wishlist.py
@@ -1,16 +1,29 @@
 from backend import db
 
-
 class Wishlist(db.Model):
+    '''Wishlist model representing a user's wishlist item.
+    
+    Attributes:
+        id (int): The unique identifier for the wishlist item.
+        user_id (int): Foreign key linking the wishlist item to a specific user.
+        book_id (int): Foreign key linking the wishlist item to a specific book.
+        book (relationship): A relationship to the Book model, representing the book in the wishlist.
+    '''
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
     book = db.relationship('Book', backref='wishlist_item')
 
     def to_dict(self):
+        '''Converts the Wishlist object into a dictionary format.
+        
+        Returns:
+            dict: A dictionary representation of the wishlist item's attributes, including the book details.
+        '''
         return {
             'id': self.id,
             'user_id': self.user_id,
             'book_id': self.book_id,
-            'book': self.book.to_dict() if self.book else None
-            }
+            'book': self.book.to_dict() if self.book else None  # Includes book details if available
+        }
