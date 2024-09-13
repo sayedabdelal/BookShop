522909beba15c20008c2e6a70520da5c295f92f4
aylaaoufi <116092909+youssef3092004@users.noreply.github.com>
2024-09-13 04:07:59 +0300

    documentation the view login

--- backend/views/login.py
+++ backend/views/login.py
@@ -1,11 +1,26 @@
 from flask import Blueprint, request, jsonify, session
 from backend.models.user import User
 
-
 login = Blueprint('login', __name__)
 
 @login.route('/login', methods=['POST'])
 def login_view():
+    '''Authenticate a user and establish a session.
+
+    This route allows users to log in by providing their email and password. 
+    On successful login, the user is assigned to a session, and a response 
+    is returned indicating whether the login was successful or not.
+
+    JSON Body:
+        email (str): The email address of the user.
+        password (str): The password of the user.
+
+    Returns:
+        Response:
+            - **200 OK**: If login is successful, returns a JSON object with a success message and user or admin ID.
+            - **400 Bad Request**: If any of the required fields (email or password) are missing.
+            - **401 Unauthorized**: If the email or password is incorrect.
+    '''
     data = request.get_json()
     email = data.get('email')
     password = data.get('password')
@@ -34,7 +49,16 @@ def login_view():
 
 @login.route('/check-session', methods=['GET'])
 def check_session():
-    '''cehck for session is active or not'''
+    '''Check if the session is active.
+
+    This route verifies if the current session is active and whether the user 
+    is authenticated as an admin or a regular user.
+
+    Returns:
+        Response:
+            - **200 OK**: If a session is active, returns a JSON object with authentication status and user or admin ID.
+            - **401 Unauthorized**: If no session is active.
+    '''
     if 'admin_id' in session:
         return jsonify({
             'isAuthenticated': True,
